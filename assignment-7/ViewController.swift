eb4831d0068749883d11820cbd35d52deea287f5
Marco Kang <kmnkit@gmail.com>
2022-02-01 19:38:42 +0900

    Initial Commit

---
+++ assignment-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment-7
+//
+//  Created by Sungwoong Kang on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
